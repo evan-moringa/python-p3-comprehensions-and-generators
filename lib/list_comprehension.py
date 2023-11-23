b75d0a27086ebba8d00eb9fb4934302c6e5eb32e
Evan Keya <evan.keya@student.moringaschool.com>
2023-11-24 00:44:50 +0300

    done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+def return_evens(sequence):
+    return [num for num in sequence if num % 2 == 0]
 
-def make_exclamation(sentence_list):
-    pass
+
+result_evens = return_evens([0, 1, 3, 5, 7, 8, 9])
+print(result_evens)  # Output: [0, 8]
+
+
+def make_exclamation(sentences):
+    return [sentence + '!' for sentence in sentences]
+
+
+result_exclamation = make_exclamation(["Hello", "I'm doing great", "Python is fun"])
+print(result_exclamation)
